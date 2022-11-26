a8ccf0a2bf5f51fb6c5d47a0a55169dd7cb09ce3
Leksa <admin@petiteweb.ru>
2022-11-26 18:18:34 +0300

    create simple project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Компонент - кусок сайта, который используется многократно
+const Card = function() {
+  return <div className="card">^_^</div>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// <br/>
+// <div> =) </div>
+// <div className="red" />
+
+ReactDOM.render(
+  <>
+    <h1>Hello <span className="mark">React</span>!</h1>
+    <div class="box">
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+    </div>
+  </>,
+  document.querySelector("#root")
+)
