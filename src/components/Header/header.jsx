030ccceba4ca0b859a6289ae14159e47f524928f
Leksa <admin@petiteweb.ru>
2022-11-30 21:55:36 +0300

    Add user simple registration

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -2,13 +2,26 @@ import React from "react";
 import "./header.css";
 
 export default () => {
+    let user = localStorage.getItem("user8");
+    const logIn = (e) => {
+        e.preventDefault();
+        let name = prompt("Как вас зовут?");
+        if (name) {
+            localStorage.setItem("user8", name);
+        }
+    }
+    const logOut = (e) => {
+        e.preventDefault();
+        localStorage.removeItem("user8");
+    }
     return <header>
         <a className="logo" href="">DogFood</a>
         <input type="search" placeholder="Поиск..." className="search"/>
         <nav className="menu">
-            <a href="">Личный кабинет</a>
-            <a href="">Войти</a>
-            <a href="">Выйти</a>
+            {/* true && true */}
+            {user && <a href="">{user}</a>}
+            {!user && <a href="" onClick={logIn}>Войти</a>}
+            {user && <a href="" onClick={logOut}>Выйти</a>}
         </nav>
     </header>
 }
