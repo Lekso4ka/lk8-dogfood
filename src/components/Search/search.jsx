0d8721b01b11760aacf4525bc80b1e965618327f
Leksa <admin@petiteweb.ru>
2022-12-24 18:30:50 +0300

    add product review show

--- src/components/Search/search.jsx
+++ src/components/Search/search.jsx
@@ -1,16 +1,20 @@
 import React, {useState} from "react";
 import "./search.css";
+import {useNavigate} from "react-router-dom";
 import {ReactComponent as SearchImg} from "./img/magnifying-glass-solid.svg";
 import {ReactComponent as CloseImg} from "./img/circle-xmark-regular.svg";
 
 export default ({data, searchGoods}) => {
+    const navigate = useNavigate();
     const [text, updateText] = useState("");
     const [searchData, setSearchData] = useState(data);
     const clearSearch = () => {
         updateText("");
         setSearchData(data);
+        searchGoods(data);
     }
     const search = (e) => {
+        navigate("/catalog");
         updateText(e.target.value);
         let arr = data.filter(el => el.name.toLowerCase().includes(e.target.value.toLowerCase()))
         setSearchData(arr);
